336140cc048d26751bad6339e88f3bcbe9a8d220
Ray Auxey <rayauxey@gmail.com>
2019-02-06 21:03:16 +0300

    Changed Pesaform Schema

--- api/models/pesaform.js
+++ api/models/pesaform.js
@@ -7,42 +7,42 @@ const pesaformSchema = new mongoose.Schema(
     level_0: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_1: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_2: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_3: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_4: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_5: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_6: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     },
     level_7: {
       type: ObjectIdType,
       ref: "User",
-      default: new ObjectId("0")
+      default: new ObjectId()
     }
   },
   {
